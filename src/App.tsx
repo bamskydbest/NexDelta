3dc60d417011647be1582564172b86f18380b47c
Mahmoud Abdulmajeed Taiye <mahmoudabdulmajeedtaiye@gmail.com>
2025-11-10 06:48:49 +0000

    fix: ensure main site loads from top when navigating from HeroCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Mainsite from "./pages/Mainsite";
 import HeroCard from "./pages/Preview";
+import ScrollToTop from "./components/ScrollToTop";
 // import MainSite from "./pages/MainSite"; 
 // import Preview from "./pages/Preview";
 
 export default function App() {
   return (
+    <>
+    <ScrollToTop/>
     <Routes>
       <Route path="/" element={<HeroCard />} />
       <Route path="/main" element={<Mainsite />} />
     </Routes>
+    </>
   );
 }
